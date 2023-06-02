031ce80aed6c1f69838a7bcf38961fea6f179e1c
zahid2873 <mahdi.zhm13@gmail.com>
2023-06-02 19:36:14 +0600

    delayed animation commit in log in page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -9,7 +9,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin {
-  Animation ? animation;
+  Animation ? animation, delayedAnimation, muchDelayedAnimation;
   AnimationController ? animationController;
 
   @override
@@ -18,6 +18,12 @@ class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin
     animationController = AnimationController(duration: Duration(seconds: 3),vsync: this);
     animation = Tween(begin: -1.0,end: 0.0)
         .animate(CurvedAnimation(parent: animationController!, curve: Curves.fastOutSlowIn));
+    delayedAnimation = Tween(begin: -1.0,end: 0.0)
+        .animate(CurvedAnimation(parent: animationController!,
+        curve: Interval(0.5, 1.0,curve: Curves.fastOutSlowIn)));
+    muchDelayedAnimation = Tween(begin: -1.0,end: 0.0)
+        .animate(CurvedAnimation(parent: animationController!,
+        curve: Interval(0.8, 1.0,curve: Curves.fastOutSlowIn)));
     animationController!.forward();
     super.initState();
   }
@@ -27,45 +33,85 @@ class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin
     return AnimatedBuilder(animation: animationController!,
       builder: (context, child){
       return Scaffold(
-        body: Transform(transform: Matrix4.translationValues(0.0,animation?.value *width, 0.0),
-        child: Center(
-            child: Container(
-              padding: EdgeInsets.all(12),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text("Login",style: TextStyle(fontSize: 20,fontWeight: FontWeight.w800),),
-                  SizedBox(height: 10,),
-                  TextField(
-                    decoration: InputDecoration(
-                      hintText: 'Username'
-                    ),
-                  ),
-                  SizedBox(height: 5,),
+        resizeToAvoidBottomInset: true,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Transform(transform: Matrix4.translationValues(animation?.value *width,0.0, 0.0),
+            child: Center(
+                child: Container(
+                  padding: EdgeInsets.all(12),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text("Login",style: TextStyle(fontSize: 20,fontWeight: FontWeight.w800),),
+                      SizedBox(height: 10,),
 
-                  TextField(
-                    decoration: InputDecoration(
-                      hintText: 'Password'
-                    ),
+                    ],
                   ),
-                  SizedBox(height: 10,),
-                  ElevatedButton(
-                      onPressed: (){}, child: Text("Login")),
-                  SizedBox(height: 5,),
-                  Text("Don't have an account."),
-                  SizedBox(height: 5,),
-                  MaterialButton(
-                    hoverColor: Colors.green,
-                    highlightColor: Colors.green,
-                    focusColor: Colors.green,
-                      shape: RoundedRectangleBorder(
-                        side: BorderSide(color: Colors.green,width: 2)
+                ),
+            ),
+            ),
+
+
+            Transform(transform: Matrix4.translationValues(delayedAnimation?.value *width, 0.0,0.0),
+              child: Center(
+                child: Container(
+                  padding: EdgeInsets.all(12),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+
+                      TextField(
+                        decoration: InputDecoration(
+                            hintText: 'Username'
+                        ),
                       ),
-                      onPressed: (){}, child: Text("Signup"))
-                ],
+                      SizedBox(height: 5,),
+
+                      TextField(
+                        decoration: InputDecoration(
+                            hintText: 'Password'
+                        ),
+                      ),
+                      SizedBox(height: 10,),
+
+                    ],
+                  ),
+                ),
               ),
             ),
-        ),
+
+
+
+            Transform(transform: Matrix4.translationValues(muchDelayedAnimation?.value *width,0.0,0.0,),
+              child: Center(
+                child: Container(
+                  padding: EdgeInsets.all(12),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+
+                      SizedBox(height: 10,),
+                      ElevatedButton(
+                          onPressed: (){}, child: Text("Login")),
+                      SizedBox(height: 5,),
+                      Text("Don't have an account."),
+                      SizedBox(height: 5,),
+                      MaterialButton(
+                          hoverColor: Colors.green,
+                          highlightColor: Colors.green,
+                          focusColor: Colors.green,
+                          shape: RoundedRectangleBorder(
+                              side: BorderSide(color: Colors.green,width: 2)
+                          ),
+                          onPressed: (){}, child: Text("Signup"))
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       );
       },
